2fa540e884c722b79ae1b54533695ef85b0dce11
Segun Francis <francis4dblues@gmail.com>
2020-12-21 23:46:02 +0100

    Initial project setup
     - Add project dependencies
     - Create fragment
     - Enable viewBinding

---
+++ app/src/main/java/com/segunfrancis/carddetails/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.segunfrancis.carddetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.segunfrancis.carddetails.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
